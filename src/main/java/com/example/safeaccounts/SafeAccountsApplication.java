96606f657f2bf239d46ba5eab572352829855bf6
Aleksey <kolchenkoav@gmail.com>
2026-09-03 21:58:51 +0300

    add: SafeAccountsApplication.java

---
+++ src/main/java/com/example/safeaccounts/SafeAccountsApplication.java
@@ -0,0 +1,11 @@
+package com.example.safeaccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeAccountsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SafeAccountsApplication.class, args);
+    }
+}
